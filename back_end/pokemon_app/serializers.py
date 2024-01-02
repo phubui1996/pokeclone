984e00fbefbcf7d5033ebba56c39d60dd32a25ef
phubui1996 <phubui1996@yahoo.com>
2024-01-02 07:51:33 -0900

    Plan to change id into pokemon_id

--- back_end/pokemon_app/serializers.py
+++ back_end/pokemon_app/serializers.py
@@ -12,4 +12,16 @@ class UserPokemonSerializer(ModelSerializer):
 
   class Meta:
     model = UserPokemon
-    fields = ['pokemon']
+    fields = ['user', 'pokemon']
+    # fields = '__all__'
+
+  def create(self, validated_data):
+    # Extract the nested Pokemon data from the validated data
+    pokemon_data = validated_data.pop('pokemon')
+    
+    # Get or create the Pokemon instance based on its ID
+    pokemon_instance, created = Pokemon.objects.get_or_create(**pokemon_data)
+    
+    # Create the UserPokemon instance
+    user_pokemon = UserPokemon.objects.create(pokemon=pokemon_instance, **validated_data)
+    return user_pokemon
