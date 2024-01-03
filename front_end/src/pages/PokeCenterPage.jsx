051031d923d2d58c3f4e9239c21b4bd79e18061c
Jkim1122 <jkimz1122@gmail.com>
2024-01-03 10:11:33 -0600

    pokecenter

--- front_end/src/pages/PokeCenterPage.jsx
+++ front_end/src/pages/PokeCenterPage.jsx
@@ -1,6 +1,41 @@
-
+import { useNavigate, useOutletContext } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { wildApi, pokeApi, teamApi } from '../components/utilities';
 
 const PokeCenterPage = () => {
+    const { pokeTeam, setPokeTeam, user } = useOutletContext()
+    const [currentPokemonHealth, setCurrentPokemonHealth] = useState(50)
+    const [currentPokemonHealthTotal, setCurrentPokemonHealthTotal] = useState(50)
+    const [currentOpponentHealth, setCurrentOpponentHealth] = useState(50)
+    const [currentOpponentHealthTotal, setCurrentOpponentHealthTotal] = useState(50)
+    const [currentPokemon, setCurrentPokemon] = useState([])
+
+    
+    const getTeam = async () => {
+        console.log("getting team")
+        try {
+            
+            teamApi.defaults.headers.common[
+                "Authorization"
+            ] = `Token ${user.Token}`;
+            
+            let response = await teamApi.get('manager/');
+            console.log("get team", response.data);
+            
+            if (response.status === 200) {
+                setPokeTeam(response.data)
+                console.log('the team', response.data[0].pokemons[0].user_pokemon.pokemon)
+                setCurrentPokemon(response.data[0].pokemons[0].user_pokemon.pokemon)
+            } else {
+                alert("Error retrieving team");
+            }
+        } catch (error) {
+            console.error("Error retrieving team:", error);
+        }
+    };
+    
+    console.log(currentPokemon)
+    console.log(pokeTeam)
 
     return (
         <div className="full_page_div">
