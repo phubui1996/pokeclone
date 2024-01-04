cb242481c90c17d8f52cfbdf5c6d670736519cc0
Claireese Underwood <claireese.underwood@gmail.com>
2024-01-04 09:43:04 -0600

    updated nav_bar to allow user to return to the main map from pokedex, pokecenter, house

--- front_end/src/components/NavBar.jsx
+++ front_end/src/components/NavBar.jsx
@@ -4,60 +4,63 @@ import { useState, useEffect } from "react";
 import pokeLogo from '../assets/PokeLogoClean.png'
 import { userApi } from "./utilities";
 
-export default function Navbar({user, setUser, isLoggedIn, setIsLoggedIn}) {
-    const [linkWords, setLinkWords] = useState('Log In')
-    const [linkWordsLink, setLinkWordsLink] = useState('/login')
-    
-    const navigate = useNavigate();
-    
-    const handleLogOut = async() => {
-      let response = await userApi.post("logout/")
-      console.log(response.status)
-      if (response.status === 204) {
-        setUser("")
-        localStorage.removeItem("token")
-        localStorage.removeItem("email")
-        delete userApi.defaults.headers.common["Authorization"]
-        setIsLoggedIn(false)
-        navigate("/landing")
-      } 
+export default function Navbar({ user, setUser, isLoggedIn, setIsLoggedIn }) {
+  const [linkWords, setLinkWords] = useState('Log In')
+  const [linkWordsLink, setLinkWordsLink] = useState('/login')
+
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    let response = await userApi.post("logout/")
+    console.log(response.status)
+    if (response.status === 204) {
+      setUser("")
+      localStorage.removeItem("token")
+      localStorage.removeItem("email")
+      delete userApi.defaults.headers.common["Authorization"]
+      setIsLoggedIn(false)
+      navigate("/landing")
     }
+  }
 
-    const signUpOrLogIn = () => {
-      console.log(window.location.href)
-      if (window.location.href === 'http://localhost:5173/signup') {
-        setLinkWords('Log In')
-        setLinkWordsLink('/login')
-      }
-      else if (window.location.href === 'http://localhost:5173/login') {
-        setLinkWords('Sign Up')
-        setLinkWordsLink('/signup')
-      }
-      else {
-        setLinkWords('')
-      }
+  const setLink = () => {
+    console.log(window.location.href)
+    if (window.location.href === 'http://localhost:5173/signup') {
+      setLinkWords('Log In')
+      setLinkWordsLink('/login')
     }
+    else if (window.location.href === 'http://localhost:5173/login') {
+      setLinkWords('Sign Up')
+      setLinkWordsLink('/signup')
+    }
+    else if (window.location.href === 'http://localhost:5173/house' || window.location.href === 'http://localhost:5173/pokecenter' || window.location.href === 'http://localhost:5173/pokedex') {
+      setLinkWords('Map')
+      setLinkWordsLink('/main')
+    }
+    else {
+      setLinkWords('')
+    }
+  }
 
-    console.log(isLoggedIn, user)
+  console.log(isLoggedIn, user)
 
-    useEffect(() => {
-      signUpOrLogIn()
-    })
+  useEffect(() => {
+    setLink()
+  })
 
   return (
     <>
       <nav className="Navbar" id='nav_bar'>
         <div className="navLinks">
-            <img id='nav_logo' src={pokeLogo}></img>
-            <Link to="/" className='nav_link'> Home </Link>
-            <Link to={linkWordsLink}> {linkWords} </Link>
-            {/* <Link to="/login"> Log In </Link> */}
-            {isLoggedIn ?
+          <img id='nav_logo' src={pokeLogo}></img>
+          <Link to="/" className='nav_link'> Home </Link>
+          <Link to={linkWordsLink} className='nav_link'> {linkWords} </Link>
+          {isLoggedIn ?
             <Link onClick={handleLogOut} className='nav_link'>Log Out</Link>
             :
             null
           }
-          </div>
+        </div>
       </nav>
     </>
   );
