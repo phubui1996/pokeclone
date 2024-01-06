c444c5d95ba19bec1acb1db0213c162dfdaaa1be
Claireese Underwood <claireese.underwood@gmail.com>
2024-01-06 15:54:55 -0600

    added assets for the main map, updated styling for the starter page and pokedex page

--- front_end/src/pages/MainMapPage.jsx
+++ front_end/src/pages/MainMapPage.jsx
@@ -1,8 +1,24 @@
 import { useNavigate, useOutletContext } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import main_map_music from '/src/assets/BackgroundMusic/PitcherPerfectTheme_Loopable.wav';
+import grasstile from '/src/assets/MapTiles/GrassTile.png';
+import flowertile1 from '/src/assets/MapTiles/FlowerTile1.png';
+import flowertile2 from '/src/assets/MapTiles/FlowerTile2.png';
+import clifftile from '/src/assets/MapTiles/CliffTile.png';
+import watertile from '/src/assets/MapTiles/WaterTile.png';
+import pokedex from '/src/assets/MapTiles/Pokedex-PNG-Photos.png';
+
 
 export default function MainMapPage() {
+    const tileMap = [flowertile2, grasstile, grasstile, flowertile1, grasstile, grasstile,
+                    grasstile, grasstile, grasstile, flowertile2, grasstile, grasstile,
+                    grasstile, flowertile1, flowertile1, grasstile, grasstile, flowertile2,
+                    grasstile, grasstile, grasstile, grasstile, grasstile, grasstile,
+                    grasstile, grasstile, grasstile, grasstile, grasstile, grasstile,
+                    grasstile, grasstile, grasstile, grasstile, grasstile, grasstile,
+                    clifftile, clifftile, clifftile, clifftile, clifftile, clifftile,
+                    watertile, watertile, watertile, watertile, watertile, watertile]
+
     const { isLoggedIn } = useOutletContext()
 
     const navigate = useNavigate()
@@ -43,30 +59,28 @@ export default function MainMapPage() {
     return (
         <div className='full_page_div'>
             <div id='main_map_div'>
+                {tileMap.map((tile, index) => (
+                    <img key={index} className='tile_map_div' src={`${tile}`}/>
+                ))}
                 <audio autoPlay src={main_map_music} loop type="audio/wav" volume='0.2'></audio>
                 <div id='overall_map'>
                     <div id='upper_div'>
                         <div id='map_house_div' onClick={handleHouse}>
-                            <img src='' />
                         </div>
                         <div id='poke_center_div' onClick={handlePokeCenter}>
-                            <img src='' />
                         </div>
                     </div>
                     <div id='main_area_div'>
                         <div className='tall_grass_div' onClick={handleTallGrass}>
-                            <img src='' />
                         </div>
                         <div id='gym_div' onClick={handleGym}>
-                            <img src='' />
                         </div>
                         <div className='tall_grass_div' onClick={handleTallGrass}>
-                            <img src='' />
                         </div>
                     </div>
                     <div id='bottom_div'>
                         <div id='main_pokedex_div'>
-                            <img id='main_pokedex_img'onClick={handlePokedex} src='https://imgs.search.brave.com/EIbEJpMXzF_hA9WLlUyE8Cnw2Y2h-RJKS_SX_FTU4PY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9waXhl/bGFydG1ha2VyLWRh/dGEtNzg3NDYyOTEx/OTMubnljMy5kaWdp/dGFsb2NlYW5zcGFj/ZXMuY29tL2ltYWdl/L2ZkN2E4N2JiODM5/NDc5NS5wbmc' />
+                            <img id='main_pokedex_img' onClick={handlePokedex} src={pokedex} />
                         </div>
                     </div>
                 </div>
--- front_end/src/pages/PokedexPage.jsx
+++ front_end/src/pages/PokedexPage.jsx
@@ -39,30 +39,28 @@ const PokedexPage = () => {
   return (
     <div className='full_page_div'>
       <audio autoPlay src={starterPageMusic} loop type="audio/wav" volume='0.2'></audio>
-      <div id='pokedex_under_div'>
-        <div id='pokedex_div'>
-          {/* <h2>Pokedex</h2> */}
-          <div id='pokedex_inner_div'>
-            {pokemons && pokemons.length > 0 ? (
-              pokemons.map((pokemon) => (
-                <PokemonCard
-                  key={pokemon.id}
-                  id={pokemon.id}
-                  name={pokemon.name}
-                  type={pokemon.type}
-                  back_img={pokemon.back_img}
-                  front_img={pokemon.front_img}
-                  move_1={pokemon.move_1}
-                  move_2={pokemon.move_2}
-                  hp={pokemon.hp}
-                  xp={pokemon.xp}
-                  lvl={pokemon.lvl}
-                />
-              ))
-            ) : (
-              <h3>No Pokemon in Pokedex</h3>
-            )}
-          </div>
+      <div id='pokedex_div'>
+        {/* <h2>Pokedex</h2> */}
+        <div id='pokedex_inner_div'>
+          {pokemons && pokemons.length > 0 ? (
+            pokemons.map((pokemon) => (
+              <PokemonCard
+                key={pokemon.id}
+                id={pokemon.id}
+                name={pokemon.name}
+                type={pokemon.type}
+                back_img={pokemon.back_img}
+                front_img={pokemon.front_img}
+                move_1={pokemon.move_1}
+                move_2={pokemon.move_2}
+                hp={pokemon.hp}
+                xp={pokemon.xp}
+                lvl={pokemon.lvl}
+              />
+            ))
+          ) : (
+            <h3>No Pokemon in Pokedex</h3>
+          )}
         </div>
       </div>
     </div>
