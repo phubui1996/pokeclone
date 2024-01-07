9e7b206c7800de7cf30a2e8e3cd1bdd17a57a9c5
Claireese Underwood <claireese.underwood@gmail.com>
2024-01-06 20:02:24 -0600

    tried out a few backgrounds for the gym page, redid the styling for the gym page and added a background for the gameover page

--- front_end/src/pages/GameOverPage.jsx
+++ front_end/src/pages/GameOverPage.jsx
@@ -8,18 +8,19 @@ const GameOverPage = () => {
     const navigate = useNavigate()
 
     return(
-        <>
+        <div className='full_page_div'>
             <div className="gameover">
                 <audio autoPlay src={gameOverPageMusic} loop type="audio/wav" volume='0.2'></audio>
                 <h1>TRASH</h1><h1>TRASH</h1><h1>TRASH</h1>
                 <img src="https://media.giphy.com/media/WLXO8OZmq0JK8/giphy.gif"/>
                 <h1>TRASH</h1><h1>TRASH</h1><h1>TRASH</h1>
-            </div>
+            
             <div className="gameover_buttons">
                 <Button onClick={navigate('/pokecenter')}>Try Again?</Button>
                 <Button onClick={navigate('/')}>New Game</Button>
             </div>  
-        </>
+            </div>
+        </div>
     )
 }
 
